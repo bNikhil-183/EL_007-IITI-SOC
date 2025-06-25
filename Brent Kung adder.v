45b06d7d12465e4135f292951865b0220c8c6c1e
bNikhil-183 <bhasurun@gmail.com>
2025-06-26 01:05:42 +0530

    Update Brent Kung adder.v

--- Brent Kung adder.v
+++ Brent Kung adder.v
@@ -1,4 +1,4 @@
-module BK (
+module brent_kung (
     input  [31:0] a,
     input  [31:0] b,
     output [31:0] sum
@@ -7,18 +7,20 @@ module BK (
 
     genvar i;
     generate
-        for (i = 0; i < 32; i = i + 1) begin
-            assign g[i] = a[i] & b[i];
-            assign p[i] = a[i] ^ b[i];
-        end
+        for (i = 0; i < 32; i = i + 1) 
+		     begin
+                assign g[i] = a[i] & b[i];
+                assign p[i] = a[i] ^ b[i];
+             end
     endgenerate
 
     assign c[0] = 1'b0;
     generate
-        for (i = 1; i < 32; i = i + 1) begin
-            assign c[i] = g[i-1] | (p[i-1] & c[i-1]);
-        end
+        for (i = 1; i < 32; i = i + 1) 
+		      begin
+               assign c[i] = g[i-1] | (p[i-1] & c[i-1]);
+              end
     endgenerate
 
     assign sum = p ^ c;
-endmodule
+endmodule 
